8799d68ee601ede322ed29c8a4970cd7a1eda9ad
Alexis Gutierrez <alexis.gutierrez@unileon.es>
2021-02-19 13:04:32 +0100

    Papers load

--- js/scripts.js
+++ js/scripts.js
@@ -6,31 +6,25 @@ function getPapers(){
         if (request.readyState === 4 && request.status === 200) {
             var type = request.getResponseHeader('Content-Type');
             if (type.indexOf("text") !== 1) {
-                return request.responseText;
+
+                publications = request.responseText.split('\n\n');
+    
+                var publicationsSpace = document.getElementById("publications");
+
+                var publication;
+
+                var i;
+                for (i = 0; i < publications.length; i++) {
+                    publication = publications[i].split('\n');
+                    var h3 = document.createElement("H3");
+                    h3.innerHTML = publication[0];
+                    publicationsSpace.append(h3);
+                }
             }
         }
     }
 }
 
-function populatePublications(){
-    
-    var publications = getPapers();
-    publications = publications.split('\n\n');
-    
-    var publicationsSpace = document.getElementById("publications");
-// Create an empty <tr> element and add it to the 1st position of the table:
-
-    var publication;
-
-    var i;
-    for (i = 0; i < publications.length; i++) {
-        publication = publications[i].split('\n');
-        var h3 = document.createElement("H3");
-        h3.innerHTML = publication[0];
-        publicationsSpace.append(h3);
-    }
-}
-
 window.onload = function() {
-    populatePublications();
+    getPapers();
 };
